ec7ad32cf4bb1571a8ca70e34aaeda84571eb8a6
sumit-borse <sumit.borse@bluepineapple.io>
2021-04-06 15:46:54 +0530

    first commit

---
+++ pages/posts/image.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+export default function Img(){
+    return(
+        <Image
+            src="/images/profile.jpg" // Route of the image file
+            height={144} // Desired size with correct aspect ratio
+            width={144} // Desired size with correct aspect ratio
+            alt="Your Name"
+        />
+    )
+}
